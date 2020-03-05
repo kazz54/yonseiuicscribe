89b6eeb5d900b8d5b36a4491fa247d72a36a2168
Elijah <develiahlee@gmail.com>
2020-03-05 21:13:50 +0900

    add changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,7 +38,6 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
-            path
           }
           fields {
             slug
