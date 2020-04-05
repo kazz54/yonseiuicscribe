deef197050f3dd9d28d4189d930e36a6901fb0ae
Elijah Lee <develijahlee@gmail.com>
2020-04-05 22:29:07 +0900

    update pageBreadcrmbs styles

--- src/components/pageBreadcrumbs.js
+++ src/components/pageBreadcrumbs.js
@@ -1,14 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
+import Arrow from "../images/arrow.svg"
 
 export default props => {
 	return (
 		<div>
-			<ul>
+			<ul className="breadcrumbs">
 				{props.crumbs.map((crumb, index) => (
-						((props.crumbs.length - index) > 1) ? 
-						<li key={index}><Link to={`/`}>{crumb}</Link></li> : 
-						<li key={index}>{crumb}</li> 
+						((props.crumbs.length - index) > 1) ?
+						<> 
+						<li key={index}><Link to={`/`}>{crumb}</Link></li>
+						<img src={Arrow} alt="UIC Scribe logo" />
+						</> : 
+						<li key={index}><span>{crumb}</span></li> 
 				))}
 			</ul>
 		</div>
