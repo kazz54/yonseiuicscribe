9c0b2bb873166ee7a3a2e387c0f4aa85688927d1
Elijah Lee <develijahlee@gmail.com>
2020-04-28 22:18:49 +0900

    finish magazines page and add recruitment page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default () => {
   return (
     <footer>
 			<p>The UIC Scribe was founded in 2006 as the official student-run newsmagazine of Underwood International College. It celebrates diversity of thinking, excellence in writing, and the freedom of self-expression.</p>
-			<a href="https://www.facebook.com/theuicscribe/" target="blank" rel="noopener noreferrer">Facebook</a>
+			<Link to={`/recruitment/`}>
+        Recruitment
+      </Link>
     </footer>
   )
 }
